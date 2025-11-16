5d27f48c5f86d99864793213a2d4149ae295eb35
IanLoi17 <ianloi382@gmail.com>
2025-11-17 02:28:41 +0800

    Updated seam carver code

--- ImageCompressor/SeamCarver.cpp
+++ ImageCompressor/SeamCarver.cpp
@@ -81,36 +81,122 @@ std::vector<int> SeamCarver::findVerticalSeam()
 	return seam;
 }
 
-void SeamCarver::removeVerticalSeam()
+std::vector<int> SeamCarver::findHorizontalSeam()
 {
-	std::vector<int> seam = findVerticalSeam();
+	int rows = energyMap.rows;
+	int cols = energyMap.cols;
 
-	cv::Mat newImg(image.rows, image.cols - 1, image.type());
-	for (int i = 0; i < image.rows; i++)
+	cv::Mat dpTable(rows, cols, CV_64F);
+	for (int i = 0; i < rows; i++)
 	{
-		int seamCol = seam[i];
-		if (seamCol > 0)
-		{
-			image.row(i).colRange(0, seamCol).copyTo(newImg.row(i).colRange(0, seamCol));
-		}
+		dpTable.at<double>(i, 0) = energyMap.at<double>(i, 0);
+	}
 
-		if (seamCol < image.cols - 1)
+	for (int j = 1; j < cols; ++j)
+	{
+		for (int i = 0; i < rows; ++i)
 		{
-			image.row(i).colRange(seamCol + 1, image.cols).copyTo(newImg.row(i).colRange(seamCol, newImg.cols));
+			double minEnergy = dpTable.at<double>(i, j - 1);
+
+			if (i > 0)
+			{
+				minEnergy = cv::min(minEnergy, dpTable.at<double>(i - 1, j - 1));
+			}
+
+			if (i < rows - 1)
+			{
+				minEnergy = cv::min(minEnergy, dpTable.at<double>(i + 1, j - 1));
+			}
+
+			dpTable.at<double>(i, j) = energyMap.at<double>(i, j) + minEnergy;
 		}
 	}
 
-	newImg.copyTo(image);
+	std::vector<int> seam(cols);
+
+	double minVal;
+	cv::Point minLoc;
+	cv::minMaxLoc(dpTable.col(cols - 1), &minVal, nullptr, &minLoc, nullptr);
+	seam[cols - 1] = minLoc.y;
+
+	for (int j = cols - 2; j >= 0; --j)
+	{
+		int currRow = seam[j + 1];
+		int nextRow = currRow;
+		double minEnergy = dpTable.at<double>(currRow, j);
+
+		if (currRow > 0 && dpTable.at<double>(currRow - 1, j) < minEnergy)
+		{
+			minEnergy = dpTable.at<double>(currRow - 1, j);
+			nextRow = currRow - 1;
+		}
+
+		if (currRow < rows - 1 && dpTable.at<double>(currRow + 1, j) < minEnergy)
+		{
+			nextRow = currRow + 1;
+		}
+
+		seam[j] = nextRow;
+	}
+
+	return seam;
+}
+
+void SeamCarver::removeSeam(std::vector<int> const& seam, bool vertical)
+{
+	if (vertical)
+	{
+		cv::Mat newImg(image.rows, image.cols - 1, image.type());
+		for (int i = 0; i < image.rows; i++)
+		{
+			int seamCol = seam[i];
+			if (seamCol > 0)
+			{
+				image.row(i).colRange(0, seamCol).copyTo(newImg.row(i).colRange(0, seamCol));
+			}
+
+			if (seamCol < image.cols - 1)
+			{
+				image.row(i).colRange(seamCol + 1, image.cols).copyTo(newImg.row(i).colRange(seamCol, newImg.cols));
+			}
+		}
+
+		newImg.copyTo(image);
+	}
+
+	else
+	{
+		cv::Mat newImg(image.rows - 1, image.cols, image.type());
+		for (int j = 0; j < image.cols; ++j)
+		{
+			int seamRow = seam[j];
+			if (seamRow > 0)
+			{
+				image.col(j).rowRange(0, seamRow).copyTo(newImg.col(j).rowRange(0, seamRow));
+			}
+
+			if (seamRow < image.rows - 1)
+			{
+				image.col(j).rowRange(seamRow + 1, image.rows).copyTo(newImg.col(j).rowRange(seamRow, newImg.rows));
+			}
+		}
+
+		newImg.copyTo(image);
+	}
+
 	calculateEnergyMap();
+}
+
+void SeamCarver::removeVerticalSeam()
+{
+	std::vector<int> seam = findVerticalSeam();
+	removeSeam(seam, true);
 }
 
 void SeamCarver::removeHorizontalSeam()
 {
-	cv::transpose(image, image);
-	calculateEnergyMap();
-	removeVerticalSeam();
-	cv::transpose(image, image);
-	calculateEnergyMap();
+	std::vector<int> seam = findHorizontalSeam();
+	removeSeam(seam, false);
 }
 
 cv::Mat SeamCarver::getImage() const
