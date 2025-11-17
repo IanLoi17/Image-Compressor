c8cd8f73fbd0349be7fc49819a501ecf6ecbf014
IanLoi17 <ianloi382@gmail.com>
2025-11-17 17:47:05 +0800

    More updates

--- ImageCompressor/main.cpp
+++ ImageCompressor/main.cpp
@@ -16,7 +16,7 @@ int main()
 
 		std::cout << "Origial size: " << originalImg.cols << "x" << originalImg.rows << std::endl;
 
-		int seamsToRemove = 50;
+		int seamsToRemove = 200;
 		std::cout << "RUNNING Dynamic Programming..." << std::endl;
 		SeamCarver carverDP(originalImg.clone());
 		auto startDP = std::chrono::high_resolution_clock::now();
@@ -47,9 +47,32 @@ int main()
 
 		cv::imwrite("Images/compressedGreedy.jpg", carverGreedy.getImage());
 
-		cv::imshow("Original", originalImg);
-		cv::imshow("Compressed (Carved DP): ", carverDP.getImage());
-		cv::imshow("Compressed (Carved Greedy): ", carverGreedy.getImage());
+		cv::Mat dpImg = carverDP.getImage();
+		cv::Mat greedyImg = carverGreedy.getImage();
+
+		if (dpImg.rows != originalImg.rows)
+		{
+			int bottomPadding = originalImg.rows - dpImg.rows;
+			cv::copyMakeBorder(dpImg, dpImg, 0, bottomPadding, 0, 0, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
+			cv::copyMakeBorder(greedyImg, greedyImg, 0, bottomPadding, 0, 0, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
+		}
+
+		cv::putText(originalImg, "Original", cv::Point(20, 50), cv::FONT_HERSHEY_SIMPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
+		cv::putText(dpImg, "DP Carved", cv::Point(20, 50), cv::FONT_HERSHEY_SIMPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
+		cv::putText(greedyImg, "Greedy Carved", cv::Point(20, 50), cv::FONT_HERSHEY_SIMPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
+
+		int thickness = 20;
+		cv::Scalar blackColor(0, 0, 0);
+
+		cv::Mat separator(originalImg.rows, thickness, originalImg.type(), blackColor);
+
+		cv::Mat combined;
+		std::vector<cv::Mat> imagesToCombine = { originalImg, separator, dpImg, separator, greedyImg };
+		cv::hconcat(imagesToCombine, combined);
+		
+		cv::namedWindow("Comparison: Original VS DP VS Greedy", cv::WINDOW_NORMAL);
+		cv::imshow("Comparison: Original VS DP VS Greedy", combined);
+
 		cv::waitKey(0);
 
 		return 0;
